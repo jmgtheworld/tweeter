695d40145e4b291a713e33dcd09317f54c58fad0
vagrant <avedj94@hotmail.com>
2020-11-17 23:02:44 +0000

    implement css and DOM change to add visualization

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#tweet-text").keyup(function() {
+    // update counter 
+    let counter = $(this).parent().children(".tweetAndcount").children(".counter")
+    counter.text(140 - $(this).val().length); 
+    // make counter red if count goes below 0  
+    if ((counter.val()) < 0) {
+      counter.addClass("error");
+    }
+    if ((counter.val()) > 0) {
+      counter.removeClass("error");
+    }
+  });
+});
+
